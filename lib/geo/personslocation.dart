2e32fd9fc397432ba0a82393f3bd3682745a03c8
mdabrowski <m.dabrowski@appnroll.com>
2018-12-07 12:31:55 +0100

    Add persons location screen

---
+++ lib/geo/personslocation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PersonsLocation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Persons location'),
+      ),
+      body: Center(
+        child: Text('Person 1')
+      ),
+    );
+  }
+}
